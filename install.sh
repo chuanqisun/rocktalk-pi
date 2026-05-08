9e3c6ef52900f1d72e0a48125a997640328e9362
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-08 17:22:26 -0400

    update systemd

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ npm init -y
 npm install spi-device rxjs @clack/prompts
 
 # register systemd service
-cp rocktalk.service /etc/systemd/system/
-systemctl enable rocktalk.service
-systemctl start rocktalk.service
+sudo cp rocktalk.service /etc/systemd/system/
+sudo systemctl enable rocktalk.service
+sudo systemctl start rocktalk.service
