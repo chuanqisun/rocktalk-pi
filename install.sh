a2549d28ccc7d8c8144afa98ff2debfd8fe72055
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-08 17:09:08 -0400

    link node

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 # Install node
 nvm install 24
 
+# Copy node binary to /usr/local/bin for systemd service access
+NODE_PATH=$(which node)
+sudo ln -s $NODE_PATH /usr/local/bin/node
+
 #  setup node dependencies
 npm init -y
 npm install spi-device rxjs @clack/prompts esbuild
