fc6b577ee2b1a13720b56aaf18baaeb9653b6a88
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-08 17:14:37 -0400

    update scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-
 # Install nvm
-NODE_BIN_URL="https://nodejs.org/dist/v24.15.0/node-v24.15.0-linux-x64.tar.xz"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
-# Install node prebuild binary to /usr/bin/node
-curl -L $NODE_BIN_URL -o node.tar.xz
-sudo tar -xf node.tar.xz --strip-components=1 -C /usr/bin/ --no-same-owner --no-same-permissions
-rm node.tar.xz
+# Install node
+nvm install 24
 
 #  setup node dependencies
 npm init -y
