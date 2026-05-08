5e834abd7e3840bc07d459ad38d782fffd4d98e7
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-08 11:38:23 -0400

    adjust stream

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ const detach$ = from(read$).pipe(
   map(([_, uid]) => ({ type: "detach", uid }))
 );
 
-const identify$ = from(read$).pipe(concatMap((result) => of({ type: "read", ...result })));
+const identify$ = from(read$).pipe(
+  distinctUntilKeyChanged("uid"),
+  concatMap((result) => of({ type: "read", ...result }))
+);
 
 const interrupt$ = merge(lastUid$, detach$).pipe(
   distinctUntilKeyChanged("uid"),
