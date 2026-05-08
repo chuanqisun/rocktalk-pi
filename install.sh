4e52e4370754f7843a8f7e87bee72815117e0077
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-08 16:43:34 -0400

    update installer

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+# Install node
+nvm install 24
+
+#  setup node dependencies
 npm init -y
 npm install spi-device rxjs @clack/prompts
+
+# register systemd service
+# TODO
